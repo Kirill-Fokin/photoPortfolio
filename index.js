9da2f51083d5cff2dc720fd6cd26d955d959b311
Kirill-Fokin <kirill19011998@mail.ru>
2023-11-03 01:54:36 +0300

    feat: add media

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const burger = document.querySelector('.header__burger-btn')
 const header = document.querySelector('.header')
 burger.addEventListener('click', () => header.classList.toggle('open'))
+
+
+
+let mail = document.querySelector('.mail')
+mail.addEventListener('click', () => console.log('mail'))
+
+
+function creareMailForm() {
+  let form = document.createElement('div')
+  form.classList.add('mail-form')
+}
