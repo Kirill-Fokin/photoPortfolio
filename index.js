22fe9e5aa2fb93e7173a3535bc653268591965c9
Kirill-Fokin <kirill19011998@mail.ru>
2023-11-03 13:52:16 +0300

    feat: add pop-up

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const header = document.querySelector('.header')
 burger.addEventListener('click', () => header.classList.toggle('open'))
 
 
+let modal = document.querySelector('.modal')
+let modalCloseBtn = document.querySelector('.modal__close-btn')
+modalCloseBtn.addEventListener('click', () => modal.classList.remove('open'))
+
 
 let mail = document.querySelector('.mail')
-mail.addEventListener('click', () => console.log('mail'))
+mail.addEventListener('click', () => modal.classList.add('open') )
+
+
+
 
 
 function creareMailForm() {
